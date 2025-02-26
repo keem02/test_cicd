0c1381eca5b867db69e210364445a892d4921435
keem02 <contradanza.90@gmail.com>
2025-02-26 15:53:20 +0900

    test

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
@@ -22,8 +23,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('books.urls')),  # 메인 페이지는 도서 목록
-    path('accounts/', include('accounts.urls')),
-    path('books/', include('books.urls')), 
+    path("admin/", admin.site.urls),
+    path("", include("books.urls")),  # 메인 페이지는 도서 목록
+    path("accounts/", include("accounts.urls")),
+    path("books/", include("books.urls")),
 ]
